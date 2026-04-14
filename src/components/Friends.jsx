da857eeb0e59f9d5ecf752736420b348b2a01074
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-04-14 22:32:55 +0600

    friends card issue fixed

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -11,7 +11,7 @@ const Friends = ({friendsPromise}) => {
 
     <h2 className="mb-4 text-(--primaryContent) font-semibold text-2xl">Your Friends</h2>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
 
       {friendsData.map((friend)=><FriendCard key={friend.id} friendData={friend} />)}
 
--- src/ui/FriendCard.jsx
+++ src/ui/FriendCard.jsx
@@ -1,24 +1,67 @@
 import { useNavigate } from "react-router";
 
-const FriendCard = ({friendData}) => {
-  
-  const {id,name,picture,days_since_contact,status,tags}=friendData
-const navigate=useNavigate()
+const FriendCard = ({ friendData }) => {
+  const { id, name, picture, days_since_contact, status, tags } = friendData;
+  const navigate = useNavigate();
+  const statusMaker = (x) => {
+    let status;
+    switch (x) {
+      case "almost due":
+        status = (
+          <span className="py-1.5 px-3 text-[12px] capitalize rounded-full font-medium text-white bg-[#EFAD44]">
+            {x}
+          </span>
+        );
 
+        break;
+
+      case "overdue":
+        status = (
+          <span className="py-1.5 px-3 text-[12px] capitalize rounded-full font-medium text-white bg-[#EF4444]">
+            {x}
+          </span>
+        );
+        break;
+
+      case "on-track":
+        status = (
+          <span className="py-1.5 px-3 text-[12px] capitalize rounded-full font-medium text-white bg-[#244D3F]">
+            {x}
+          </span>
+        );
+        break;
+    }
+
+    return status;
+  };
   return (
-    <div className="py-6 px-4 text-center space-y-3 rounded-lg bg-base-100 shadow" onClick={()=>navigate(`/friends/details/${id}`)}>
-
-      <div><img src={picture} alt={name} className="mx-auto"/></div>
-      <div className="space-y-2">
-        <h4 className="text-(--primaryContent) text-xl font-semibold">{name}</h4>
-<p className="text-[12px] text-(--textColor)">{days_since_contact}d ago</p>
-  <div className="flex flex-wrap gap-2 justify-center items-center">
-    {tags.map((x,inx)=><span key={inx} className="py-1.5 px-3 text-[12px] uppercase rounded-full font-medium text-(--primaryColor) bg-(--greenSecondary)" >{x}</span>)}
-  </div>
-
-       <span >Status</span> 
+    <div
+      className="p-6 text-center space-y-3 rounded-lg bg-base-100 shadow"
+      onClick={() => navigate(`/friends/details/${id}`)}
+    >
+      <div>
+        <img src={picture} alt={name} className="mx-auto" />
       </div>
+      <div className="space-y-2">
+        <h4 className="text-(--primaryContent) text-xl font-semibold">
+          {name}
+        </h4>
+        <p className="text-[12px] text-(--textColor)">
+          {days_since_contact}d ago
+        </p>
+        <div className="flex flex-wrap gap-2 justify-center items-center ">
+          {tags.map((x, inx) => (
+            <span
+              key={inx}
+              className="py-1.5 px-3 text-[12px] uppercase rounded-full font-medium text-(--primaryColor) bg-(--greenSecondary)"
+            >
+              {x}
+            </span>
+          ))}
+        </div>
 
+        <div className="pt-1">{statusMaker(status)}</div>
+      </div>
     </div>
   );
 };
